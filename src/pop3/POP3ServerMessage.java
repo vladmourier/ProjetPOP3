7f91749e76b81d47fcf395e77f8eecf5d0b574d7
vladmourier <vladmourier@hotmail.fr>
2016-03-07 10:42:32 +0100

    Initialisation du projet

---
+++ src/pop3/POP3ServerMessage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pop3;
+
+/**
+ *
+ * @author Vladimir
+ */
+public class POP3ServerMessage {
+    public String message;
+    public boolean isOk;
+    
+    
+}
