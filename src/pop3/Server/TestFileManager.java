7d18580e61be19684eebf0849da2b9d36ee13984
Adrien <Adrien@134.214.119.125>
2016-03-15 09:59:59 +0100

    Filemanager User pass getMails deleteMails (encours)

---
+++ src/pop3/Server/TestFileManager.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pop3.Server;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Adrien
+ */
+public class TestFileManager {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FileManager FM = new FileManager();
+        try {
+            System.out.println("Le user Adrien a pour id : "+FM.findUser("Adrien"));
+            System.out.println("Le pass est : "+FM.verifyPass(3, "motdepasse"));
+            System.out.println("Le pass est : "+FM.verifyPass(3, "test"));
+            FM.getMails(1);
+            FM.deleteMail(1, 1);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(TestFileManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
